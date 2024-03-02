d57b5a50f718465fe2032f2e6e6d144971b59b03
Miles <wzq429512@alibaba-inc.com>
2024-03-02 20:55:45 +0800

    add: some_fix

--- test.js
+++ test.js
@@ -1,80 +1,10 @@
-// function Promise(fn) {
-//     var value = null,
-//         callbacks = [];  //callbacks为数组，因为可能同时有很多个回调
-
-//     this.then = function (onFulfilled) {
-//         callbacks.push(onFulfilled);
-//     };
-
-//     function resolve(value) {
-//         callbacks.forEach(function (callback) {
-//             callback(value);
-//         });
-//     }
-
-//     fn(resolve);
-// }
-
-
-
-
-// const p = new Promise((resolve, reject) => {
-//     console.log('promise-start')
-//     resolve('asdasd')
-//     reject('xxx')
-// })
-
-// p.then((res) => {
-//     console.log('res>>', res)
-// })
-
-// p.catch((err) => {
-//     console.log('err>>', err)
-// })
-
-
-
-class MyPromise {
-    constructor(exector) {
-        this.initData()
-        this.initBind()
-        exector(this.resolve, this.reject)
-    }
-    initData() {
-        this.promiseResult = ''
-        this.promiseStatus = 'pending'
-    }
-    initBind () {
-        // 初始化this
-        this.resolve = this.resolve.bind(this)
-        this.reject = this.reject.bind(this)
-    }
-    resolve(data) {
-        if (this.promiseStatus === 'pending') {
-            this.promiseStatus = 'resolve'
-            this.promiseResult = data
-        }
-    }
-    reject(data) {
-        if (this.promiseStatus === 'pending') {
-            this.promiseStatus = 'reject'
-            this.promiseResult = data
-        }
-    }
-    then(callback) {
-        callback(this.promiseResult)
-    }
-    catch(callback) {
-        callback(this.promiseResult)
-    }
-    
-}
-
-const p = new MyPromise((resolve, reject) => {
-    console.log('promise-start')
-    // resolve('asdasd')
-    reject('xxx')
-})
-p.then((res) => {
-    console.log('res>>>', res)
-})
+/*
+ * @Author: 佚心
+ * @Date: 2023-12-28 15:50:04
+ * @Description: 
+ */
+// main.js
+console.log('main starting');
+const a = require('./a.js');
+const b = require('./b.js');
+console.log('in main, a.done=%j, b.done=%j', a.done, b.done);

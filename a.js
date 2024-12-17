4702983e6c7d48943d06aa232eeaea6d28dfca9c
didadida262 <719713496@qq.com>
2024-12-17 16:59:49 +0800

    add: vercel_json

--- a.js
+++ a.js
@@ -4,9 +4,13 @@
  * @Description: 
  */
 // a.js
-console.log('a starting');
+console.log("a starting");
 exports.done = false;
-const b = require('./b.js');
-console.log('in a, b.done = %j', b.done);
+const b = require("./b.js");
+
+console.log("in a, b.done = %j", b.done);
 exports.done = true;
-console.log('a done');
+console.log("a done");
+
+// https://didadida262.github.io/2024/10/16/canvas%E7%A6%BB%E5%B1%8F%E6%B8%B2%E6%9F%93%E8%AF%A6%E8%A7%A3/
+// https://hhvcg-blog.vercel.app/2024/10/16/canvas%E7%A6%BB%E5%B1%8F%E6%B8%B2%E6%9F%93%E8%AF%A6%E8%A7%A3/
